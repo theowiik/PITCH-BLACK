86ee69deb4c5af3393780ac0ee72a832a24a1449
theowiik <theo.le.wiik@gmail.com>
2020-03-14 16:28:14 +0100

    add basic movement

--- scripts/Actor.gd
+++ scripts/Actor.gd
@@ -1,4 +1,17 @@
 extends KinematicBody2D
 
-func _ready():
-	pass
+const movement_speed: int = 100
+
+func _process(_delta) -> void:
+	move_and_slide(get_input_vector() * movement_speed)
+
+# Returns a normalized input vector.
+func get_input_vector() -> Vector2:
+	var vec = Vector2(0,0)
+
+	if Input.is_action_pressed("up"): vec.y -= 1
+	if Input.is_action_pressed("down"): vec.y += 1
+	if Input.is_action_pressed("right"): vec.x += 1
+	if Input.is_action_pressed("left"): vec.x -= 1
+
+	return vec.normalized()
